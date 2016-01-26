5f0b8a5386545761f45fb4b6777e5a311a36755b
afreeman123 <freeman.aidan@gmail.com>
2016-01-26 18:45:40 +0000

    First 5 tutorials

---
+++ Aidan'sFirstProject/Aidan'sFirstProject/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aidan_sFirstProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //object can be any var type
+            object myObj = true;
+            MessageBox.Show(myObj.ToString());
+
+            //changes text property of button1
+            button1.Text = "Aidan";
+
+            button1.Height = 60;
+
+            textBox1.Text = "Aidan";
+            textBox1.MaxLength = 2;
+
+            //disables button
+            button1.Enabled = false;
+        }
+
+        private void button2_MouseHover(object sender, EventArgs e)
+        {
+            MessageBox.Show("You are hovering your mouse over the button");
+        }
+
+    }
+        
+}
